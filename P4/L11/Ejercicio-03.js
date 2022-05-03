b5c66599a28493d33d55b434bf3919fe3931688f
dberganciano <d.berganciano.2019@alumnos.urjc.es>
2022-05-03 10:33:40 +0200

    L11

---
+++ P4/L11/Ejercicio-03.js
@@ -0,0 +1,86 @@
+console.log("Ejecutando JS....")
+
+// Obtener elementos del DOM
+const canvas = document.getElementById('canvas');
+const img = document.getElementById('imagesrc');
+const ctx = canvas.getContext('2d');
+
+// Constante deslizadores colores
+const desRed = document.getElementById('red');
+const desGreen = document.getElementById('green');
+const desBlue = document.getElementById('blue');
+
+// Botones
+const bColores = document.getElementById('colores');
+
+// Valor del deslizador
+const range_value_red = document.getElementById('range_red');
+const range_value_green = document.getElementById('range_green');
+const range_value_blue = document.getElementById('range_blue');
+
+// Ocultar los deslizadores inicialmente
+document.getElementById('sliders').style.display = 'none';
+
+img.onload = function() {
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  ctx.drawImage(img, 0,0);
+  colores();
+};
+
+  function filtroColores(data) { 
+    umbralRed = desRed.value
+    umbralGreen = desGreen.value
+    umbralBlue = desBlue.value
+
+    for (var i = 0; i < data.length; i+=4) {
+      if (data[i] > umbralRed) {
+        data[i] = umbralRed;
+      }
+      if (data[i+1] > umbralGreen) {
+        data[i+1] = umbralGreen;
+      }
+      if (data[i+2] > umbralBlue) {
+        data[i+2] = umbralBlue;
+      }
+    }
+  }
+
+function colores() {
+  ctx.drawImage(img, 0,0);
+
+  desRed.oninput = () => {
+    range_value_red.innerHTML = desRed.value;
+    ctx.drawImage(img, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data;
+    filtroColores(data);
+    ctx.putImageData(imgData, 0, 0);
+  }
+
+  desGreen.oninput = () => {
+    range_value_green.innerHTML = desGreen.value;
+    ctx.drawImage(img, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data
+    filtroColores(data);
+    ctx.putImageData(imgData, 0, 0);
+  }
+
+  desBlue.oninput = () => {
+    range_value_blue.innerHTML = desBlue.value;
+    ctx.drawImage(img, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data;
+    filtroColores(data);
+    ctx.putImageData(imgData, 0, 0);
+  }
+}
+
+bColores.onclick = () => {
+    ctx.drawImage(img, 0,0);
+    document.getElementById('sliders').style.display = 'block';
+}
+
+console.log("Fin...");
